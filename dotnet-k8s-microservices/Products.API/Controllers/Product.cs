eb11dc930015ff6682b9e91823f6c5a9db50bae0
Ammar Gangardiwala <ammar.gangardiwala@cognizant.com>
2020-04-29 14:04:31 -0500

    product api impl

---
+++ dotnet-k8s-microservices/Products.API/Controllers/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Products.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Orange", "Apple", "PineApple"
+        };
+
+        private readonly ILogger<Product> _logger;
+
+        public ProductController(ILogger<Product> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 3).Select(index => new Product
+            {
+                Id = Guid.NewGuid().ToString(),
+                ProductName = Summaries[index-1]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ dotnet-k8s-microservices/Products.API/Product.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Products.API
+{
+    [Serializable]
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string ProductName { get; set; }
+    }
+}
