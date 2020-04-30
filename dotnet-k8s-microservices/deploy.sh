47d7f2213b4952b894f465062fac00c8fcb104ee
mturaga <maruthi.turaga@gmail.com>
2020-04-29 22:11:26 -0500

    add chmod

--- dotnet-k8s-microservices/deploy.sh
+++ dotnet-k8s-microservices/deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-# set -ev
+set -ev
 
-# TAG=$1
-# DOCKER_USERNAME=$2
-# DOCKER_PASSWORD=$3
+TAG=$1
+DOCKER_USERNAME=$2
+DOCKER_PASSWORD=$3
 
-# # Create publish artifact
-# dotnet publish -c Release src
+# Create publish artifact
+dotnet publish -c Release src
 
-# # Build the Docker images
-# docker build -t repository/project:$TAG src/bin/Release/netcoreapp1.0/publish/.
-# docker tag repository/project:$TAG repository/project:latest
+# Build the Docker images
+docker build -t repository/project:$TAG src/bin/Release/netcoreapp1.0/publish/.
+docker tag repository/project:$TAG repository/project:latest
 
-# # Login to Docker Hub and upload images
-# docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-# docker push repository/project:$TAG
-# docker push repository/project:latest
-
-echo "HELLO FROM deploy sh"
+# Login to Docker Hub and upload images
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push repository/project:$TAG
+docker push repository/project:latest
