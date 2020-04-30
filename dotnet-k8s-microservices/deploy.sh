e25e82fa05e5d3a391b9356812bf74c8b24be348
mturaga <maruthi.turaga@gmail.com>
2020-04-29 22:45:36 -0500

    test

--- dotnet-k8s-microservices/deploy.sh
+++ dotnet-k8s-microservices/deploy.sh
@@ -4,14 +4,16 @@ TAG=$1
 DOCKER_USERNAME=$2
 DOCKER_PASSWORD=$3
 
+echo $1
+
 # Create publish artifact
 dotnet publish -c Release src
 
-# Build the Docker images
-docker build -t repository/project:$TAG src/bin/Release/netcoreapp1.0/publish/.
-docker tag repository/project:$TAG repository/project:latest
+# # Build the Docker images
+# docker build -t repository/project:$TAG src/bin/Release/netcoreapp1.0/publish/.
+# docker tag repository/project:$TAG repository/project:latest
 
-# Login to Docker Hub and upload images
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push repository/project:$TAG
-docker push repository/project:latest
+# # Login to Docker Hub and upload images
+# docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+# docker push repository/project:$TAG
+# docker push repository/project:latest
